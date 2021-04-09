781da856f4a582f3e16a1f44bd39f09557bb6ae0
mahdizaabi <mahdi.zaabi@gmail.com>
2021-04-10 00:03:44 +0200

    add utlity

--- utils/getContactEmail.js
+++ utils/getContactEmail.js
@@ -1,5 +1,11 @@
-const getContactEmail = (users, userLoggedIn) => 
-    users?.filter((userToFilter) => userToFilter !== userLoggedIn?.email)[0];   
+/***
+ * getContactEmail: pick up the contact email
+ * @users: list of 2 items(eemails): user logedin and contact user
+ * @ userLoggedIn: the current logged user
+ * Return: The contact email
+ *
+ ***/
+const getContactEmail = (users, userLoggedIn) =>
+  users?.filter((userToFilter) => userToFilter !== userLoggedIn?.email)[0];
 
 export default getContactEmail;
-
