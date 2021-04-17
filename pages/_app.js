0d1e4fbc61fb855f8d1a14b7dcd19a2dfed6a057
mahdizaabi <mahdi.zaabi@gmail.com>
2021-04-17 21:23:39 +0200

    add

--- pages/_app.js
+++ pages/_app.js
@@ -7,20 +7,18 @@ import { useEffect, useState } from "react";
 import Loading from "../components/loading";
 import "../components/loading.css";
 import firebase from "firebase";
-import '../components/SmallChatBox.css';
-import "../components/chatsScreen.css"
-import "./index.css"
+import "../components/SmallChatBox.css";
+import "../components/chatsScreen.css";
+import "./index.css";
 import { useCollection } from "react-firebase-hooks/firestore";
-import "../pages/chat/chat.css"
-import Fstore from "../components/paracticefs";
+import "../pages/chat/chat.css";
+import "../components/message.css";
 
 function MyApp({ Component, pageProps }) {
   const [isSignedIn, setIsSignedIn] = useState(null);
   const [isLaoding, setLoading] = useState(true);
-  
 
   useEffect(() => {
-   
     auth.onAuthStateChanged(function (user) {
       if (user) {
         setIsSignedIn(true);
@@ -34,7 +32,7 @@ function MyApp({ Component, pageProps }) {
           { merge: true }
         );
       } else {
-        setIsSignedIn(false)
+        setIsSignedIn(false);
         setLoading(false);
       }
     });
@@ -45,7 +43,6 @@ function MyApp({ Component, pageProps }) {
       <Loading isLaoding={isLaoding}></Loading>
       <Login isSignedIn={isSignedIn} laoding={isLaoding}></Login>
       {isSignedIn ? <Component {...pageProps} /> : ""}
-      <Fstore></Fstore>
     </div>
   );
 }
--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -6,14 +6,14 @@ import { auth, db } from "../../firebase";
 import getContactEmail from "../../utils/getContactEmail";
 
 function ChatPage({ chatMetaDada, messages, fake }) {
-  console.log(`messages: ${messages}`)
+  console.log(`messages: ${messages}`);
 
   const [user] = useAuthState(auth);
   return (
     <div className="chatPage__container">
       <Head>
         <title>chat with {getContactEmail(chatMetaDada.users, user)} </title>
-      </Head>  
+      </Head>
       <Sidebar />
       <div className="chatbox__container">
         <div className="chatPage__Screen">
@@ -25,24 +25,27 @@ function ChatPage({ chatMetaDada, messages, fake }) {
 }
 
 export default ChatPage;
+
 export async function getServerSideProps(context) {
   const ref = db.collection("chats").doc(context.query.id);
- 
+
   const messagesResponse = await ref
     .collection("messages")
     .orderBy("timestamp", "asc")
     .get();
 
   /*  map first through the message and return all the messages and document id of the chat fileed iside of it  */
-  
-/* the returned aray from the first map, map it over it again and convert it's timestamp */
-const messages = messagesResponse.docs.map((doc) => ({
-  id: doc.id,
-  ...doc.data(),
-})).map((messages) => ({
-  ...messages,
-  timestamp: messages.timestamp.toDate().getTime(),
-}))
+
+  /* the returned aray from the first map, map it over it again and convert it's timestamp */
+  const messages = messagesResponse.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    .map((messages) => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime(),
+    }));
 
   const chatRes = await ref.get();
   /* id: document id    
